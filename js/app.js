5423cd9dbcab22e80f7ec19f724124e30dc8e3fa
CristianGomes <cristianwalace@gmail.com>
2023-09-24 22:18:30 -0300

    JS do botão ver mais

--- js/app.js
+++ js/app.js
@@ -13,18 +13,32 @@ cardapio.eventos = {
 cardapio.metodos = {
 
     //Obtem a lista de itens do cardápio
-    obterItensCardapio: (categoria ='burgers') => {
+    obterItensCardapio: (categoria ='burgers', vermais = false)=> {
         
         var filtro = MENU[categoria];
         console.log(filtro)
-        $("#itensCardapio").html('') //Para limpar o cardápio antes de chamar
+
+        if(!vermais) {
+            $("#itensCardapio").html('') //Para limpar o cardápio antes de chamar
+            $("#btnVerMais").removeClass('hidden');
+        }
 
         $.each(filtro, (i, e) => {
 
             let temp = cardapio.templates.item.replace(/\${img}/g, e.img)
             .replace(/\${nome}/g, e.name)   //toFixed(2) para colocar duas casas decimais e replace para trocar . por ,
             .replace(/\${preco}/g, e.price.toFixed(2).replace('.', ','))
-            $("#itensCardapio").append(temp) 
+
+            //botão ver mais foi clicado (12 itens)
+            if (vermais && i >= 8 && i < 12) {
+                $("#itensCardapio").append(temp)
+            }
+
+            //Paginação inicial (8 itens)
+            if (!vermais && i < 8){
+                $("#itensCardapio").append(temp)
+            }
+             
         })
 
         // Remove o ativo dos botões 
@@ -32,6 +46,14 @@ cardapio.metodos = {
         
         // Seta o botão para ativo 
         $("#menu-" + categoria).addClass('active')
+    },
+
+    // Clique no botão de ver mais
+    verMais: () => {
+        var ativo = $(".container-menu a.active").attr('id').split('menu-')[1];  // posição 0 [menu-] posição 1 [burgers]
+        cardapio.metodos.obterItensCardapio(ativo, true);
+
+        $("#btnVerMais").addClass('hidden');
     },
 
 }
