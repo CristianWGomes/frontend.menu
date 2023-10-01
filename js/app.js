b926988373e7fa964b041e029deb07af29a546a6
Cristian Gomes <cristianwalace@gmail.com>
2023-10-01 17:53:46 -0300

    Função ModalFull e ativação do carrinho

--- js/app.js
+++ js/app.js
@@ -114,13 +114,62 @@ cardapio.metodos = {
                     MEU_CARRINHO.push(item[0])
                 }
 
-                alert('Item adicionado ao carrinho')
+                cardapio.metodos.mensagem('item adicionado ao carrinho', 'green');
                 $("#qntd-" + id).text(0)
+
+                cardapio.metodos.atualizarBadgeTotal();
                 
             }
 
         }
 
+    },
+
+    //atualiza o badge de totais dos botões "meu carrinho"
+    atualizarBadgeTotal: () => {
+        var total = 0;
+
+        $.each(MEU_CARRINHO, (i, e) => {
+            total += e.qntd
+        })
+        
+        if (total > 0) {
+            $(".botao-carrinho").removeClass('hidden');
+            $(".container-total-carrinho").removeClass('hidden');
+        }
+        else {
+            $(".botao-carrinho").addClass('hidden');
+            $(".container-total-carrinho").removeClass('hidden');
+        }
+
+        $(".badge-total-carrinho").html(total);
+
+
+    },
+
+
+    //abre a modal de carrinho
+    abrirCarrinho: (abrir) => {
+
+        if (abrir) {
+            $("#modalCarrinho").removeClass('hidden');
+        }
+        else {
+            $("#modalCarrinho").addClass('hidden');
+        }
+    },
+
+    mensagem: (texto, cor = 'red', tempo = 3500) => {
+
+        let id = Math.floor(Date.now() * Math.random()).toString();
+
+        let msg = `<div id="msg-${id}" class="w3-center w3-animate-top toast ${cor}">${texto}</div>`;
+
+        $("#container-mensagens").append(msg);
+        
+        setTimeout(() => {
+            $("#msg-" + id).remove();
+        }, tempo)
     }
 
 }
